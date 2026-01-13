30e504c79127d0b3a10343c93e2e0fbb6f1be4bc
rotaerCehT <thecreatorsdetails@gmail.com>
2026-01-13 21:06:37 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+(() => {
+  const inputs = {
+    'open-sesame': 'https://www.youtube.com/channel/UCsR0_oG3hOkFmh6A5DkUIhw'
+  };
+  const form = document.getElementById('secretForm');
+  const input = document.getElementById('secretInput');
+
+  function doRedirect(url) {
+    window.location.assign(url);
+  }
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const val = input.value.trim().toLowerCase();
+    if (inputs[val]) {
+      doRedirect(inputs[val]);
+    }
+  });
+
+  input.addEventListener('input', () => {
+    const val = input.value.trim().toLowerCase();
+    if (inputs[val]) doRedirect(inputs[val]);
+  });
+})();
